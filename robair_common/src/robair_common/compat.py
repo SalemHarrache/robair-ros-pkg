a8219e107035a45278b0b8793f9bab6710e6394b
Salem Harrache <salem@harrache.info>
2013-03-10 18:24:54 +0100

    add robair_common package to gather common functions

---
+++ robair_common/src/robair_common/compat.py
@@ -0,0 +1,88 @@
+# coding: utf8
+"""
+    robair_common.compat
+    --------------------
+
+    Workarounds for compatibility with Python 2 and 3 in the same code base.
+"""
+
+import sys
+
+# -------
+# Pythons
+# -------
+
+# Syntax sugar.
+_ver = sys.version_info
+
+#: Python 2.x?
+is_py2 = (_ver[0] == 2)
+
+#: Python 3.x?
+is_py3 = (_ver[0] == 3)
+
+#: Python 3.0.x
+is_py30 = (is_py3 and _ver[1] == 0)
+
+#: Python 3.1.x
+is_py31 = (is_py3 and _ver[1] == 1)
+
+#: Python 3.2.x
+is_py32 = (is_py3 and _ver[1] == 2)
+
+#: Python 3.3.x
+is_py33 = (is_py3 and _ver[1] == 3)
+
+#: Python 3.4.x
+is_py34 = (is_py3 and _ver[1] == 4)
+
+#: Python 2.7.x
+is_py27 = (is_py2 and _ver[1] == 7)
+
+#: Python 2.6.x
+is_py26 = (is_py2 and _ver[1] == 6)
+
+# ---------
+# Specifics
+# ---------
+
+if is_py2:
+    if is_py26:
+        from logging import Handler
+
+        class NullHandler(Handler):
+            def emit(self, record):
+                pass
+
+        from ordereddict import OrderedDict
+    else:
+        from logging import NullHandler
+        from collections import OrderedDict
+
+    from StringIO import StringIO
+
+    def to_char(string):
+        if len(string) == 0:
+            return bytes('')
+        return bytes(string[0])
+
+    bytes = str
+    str = unicode
+    stdout = sys.stdout
+    xrange = xrange
+
+
+elif is_py3:
+    from logging import NullHandler
+    from collections import OrderedDict
+    from io import StringIO
+
+    def to_char(string):
+        if len(string) == 0:
+            return str('')
+        return str(string[0])
+
+    str = str
+    bytes = bytes
+    stdout = sys.stdout.buffer
+    xrange = range
---
+++ robair_common/src/robair_common/logger.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+    robair_common.logger
+    --------------------
+
+    Logging setup.
+"""
+from __future__ import unicode_literals
+import logging
+import roslib
+import rospy
+from .compat import NullHandler
+
+roslib.load_manifest('robair_common')
+
+
+def get_logger():
+    '''Initialize a speaking logger with stream handler (stderr).'''
+    logger = logging.getLogger("robair")
+    level_name = rospy.get_param('logger_level')
+    if level_name not in ["CRITICAL", "ERROR", "WARN", "INFO", "DEBUG"]:
+        logger.addHandler(NullHandler())
+        return logger
+    else:
+        level = logging._levelNames[level_name]
+    # Default to logging to stderr.
+    format = '%(asctime)s %(levelname)s: %(message)s'
+    logging.basicConfig(level=level, format=format)
+
+    formatter = logging.Formatter(format)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger = logging.getLogger("robair")
+    logger.setLevel(level)
+    logger.addHandler(stream_handler)
+    return logger
+
+
+LOGGER = get_logger()
