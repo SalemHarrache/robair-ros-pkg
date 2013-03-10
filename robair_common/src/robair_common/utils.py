186d87646626902d77bb04f8b6d5def6c8836026
Salem Harrache <salem@harrache.info>
2013-03-10 20:16:38 +0100

    Parse a list of arguments and create *args and **kwargs >>> parse_args(("a", "b")) (['a', 'b'], {}) >>> parse_args(("a", "b=a")) (['a'], {'b': 'a'})

--- robair_common/src/robair_common/utils.py
+++ robair_common/src/robair_common/utils.py
@@ -66,3 +66,21 @@ class ThreadPool(object):
         """ Wait for the all threads to be completed """
         self.tasks.join()
         self.pool.join()
+
+
+def parse_args(args):
+    """ Parse a list of arguments and create *args and **kwargs
+    >>> parse_args(("a", "b"))
+    (['a', 'b'], {})
+    >>> parse_args(("a", "b=a"))
+    (['a'], {'b': 'a'})
+    """
+    new_args = []
+    kwargs = {}
+    for arg in args:
+        if "=" in arg:
+            key, value = arg.split('=', 1)
+            kwargs[key] = value
+        else:
+            new_args.append(arg)
+    return new_args, kwargs
