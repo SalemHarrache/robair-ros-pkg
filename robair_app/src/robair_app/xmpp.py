500f1d46d2022aa9e1d636444ac4d2a6380c861c
Salem Harrache <salem@harrache.info>
2013-03-10 20:15:16 +0100

    clean botxmpp and botcmd mechanism

--- robair_app/src/robair_app/xmpp.py
+++ robair_app/src/robair_app/xmpp.py
@@ -2,19 +2,20 @@ import logging
 import rospy
 from sleekxmpp import ClientXMPP
 import cPickle
-import thread
 import inspect
+import traceback
 from robair_msgs.msg import Command
 from robair_common.logger import LOGGER
+from robair_common.utils import ThreadPool
 
 
 def botcmd(*args, **kwargs):
     """Decorator for bot command functions"""
 
-    def decorate(func, name=None, thread=False):
-        setattr(func, '_jabberbot_command', True)
-        setattr(func, '_jabberbot_command_name', name or func.__name__)
-        setattr(func, '_jabberbot_command_thread', thread)  # Experimental!
+    def decorate(func, name=None, threaded=True):
+        setattr(func, '_command', True)
+        setattr(func, '_command_name', name or func.__name__)
+        setattr(func, '_threaded', threaded)  # Experimental!
         return func
 
     if len(args):
@@ -26,83 +27,75 @@ def botcmd(*args, **kwargs):
 class BotXMPP(ClientXMPP):
     MSG_ERROR_OCCURRED = 'Sorry for your inconvenience. '\
                          'An unexpected error occurred.'
-    MSG_UNKNOWN_COMMAND = 'Unknown command: %s". \n'\
-                          'Type help for available commands.'
+    MSG_UNKNOWN_COMMAND = 'Unknown command: %s". \n'
 
-    def __init__(self, jid, password, node_name):
+    def __init__(self, jid, password, node_name, num_thread=2):
         super(BotXMPP, self).__init__(jid, password)
         rospy.init_node(node_name)
         self.log = logging.getLogger(__name__)
-        self.add_event_handler("session_start", self.session_start)
-        self.add_event_handler("message", self.message_handler)
+        self.add_event_handler("session_start", self._session_start)
+        self.add_event_handler("message", self._message_handler)
         self.load_plugin()
 
         self.commands = {}
         for name, value in inspect.getmembers(self, inspect.ismethod):
-            if getattr(value, '_jabberbot_command', False):
-                name = getattr(value, '_jabberbot_command_name')
+            if getattr(value, '_command', False):
+                name = getattr(value, '_command_name')
                 LOGGER.info('Registered command: %s' % name)
                 self.commands[name] = value
+        self.threads_pool = ThreadPool(num_thread)
 
-        logging.basicConfig()
-        # import pdb; pdb.set_trace()
-
-    def session_start(self, event):
+    def _session_start(self, event):
         self.send_presence()
         self.get_roster()
 
-    def message_handler(self, msg):
-        LOGGER.debug("msg['type'] = %s" % msg['type'])
+    def _execute_and_send(self, func, msg, args, kwargs):
+        """ Execute command and reply with the result. """
+        def task(func, args, kwargs):
+            try:
+                result = func(*args, **kwargs)
+                msg.reply("%s" % result).send()
+            except Exception as e:
+                exception = traceback.format_exc(e)
+                LOGGER.exception('An error happened while processing '
+                                 'a message ("%s") from %s: %s"' %
+                                 (msg['body'], msg['from'], exception))
+                msg.reply("%s\n" % (self.MSG_ERROR_OCCURRED)).send()
+
+        if func._threaded:
+            self.threads_pool.add_task(task, args, kwargs)
+        else:
+            task(func, args, kwargs)
+
+    def _message_handler(self, msg):
         if (msg['type'] not in ('chat', 'normal')
                 or msg['body'] == ''):
             return
         msg_parts = msg['body'].split(' ')
         cmd, args = msg_parts[0], msg_parts[-1:] if len(msg_parts) > 1 else []
-        LOGGER.debug("cmd : %s :: args : %s" % (cmd, args))
+        kwargs = {}
+        LOGGER.debug("cmd : %s :: args : %s :: kwargs : %s" %
+                     (cmd, args, kwargs))
+
         if cmd in self.commands:
-            def execute_and_send():
-                try:
-                    msg.reply(self.commands[cmd](*args)).send()
-                except Exception:
-                    msg.reply(self.MSG_ERROR_OCCURRED).send()
-
-            # Experimental!
-            # if command should be executed in a seperate thread do it
-            if self.commands[cmd]._jabberbot_command_thread:
-                thread.start_new_thread(execute_and_send, ())
-            else:
-                execute_and_send()
+            func = self.commands[cmd]
+            self._execute_and_send(func, msg, args, kwargs)
         else:
-            pass
-            # # In private chat, it's okay for the bot to always respond.
-            # # In group chat, the bot should silently ignore commands it
-            # # doesn't understand or aren't handled by unknown_command().
-            # if type == 'groupchat':
-            #     default_reply = None
-            # else:
-            #     default_reply = self.MSG_UNKNOWN_COMMAND % {
-            #         'command': cmd,
-            #         'helpcommand': 'help',
-            #     }
-            # reply = self.unknown_command(mess, cmd, args)
-            # if reply is None:
-            #     reply = default_reply
-            # if reply:
-            #     self.send_simple_reply(mess, reply)
-
-        # print(msg['body'])
-        # topic_data = cPickle.loads(msg)
-        # self.pub = rospy.Publisher(topic_data.topic, topic_data.data)
+            msg.reply(self.MSG_UNKNOWN_COMMAND % cmd).send()
 
     def send_message(self, dest, mbody):
         super(BotXMPP, self).send_message(mto=dest, mbody=mbody, mtype='chat')
 
-    def load_plugin(self):
+    def _load_plugin(self):
         self.register_plugin('xep_0030')  # Service Discovery
         self.register_plugin('xep_0004')  # Data Forms
         self.register_plugin('xep_0060')  # PubSub
         self.register_plugin('xep_0199')  # XMPP Ping
         self.auto_reconnect = True
+
+    def wait_completion(self):
+        self.thread_pool.wait_completion()
+        self.disconnect()
 
 
 class RobBot(BotXMPP):
