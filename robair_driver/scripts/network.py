f1607861fe47756d5ad8b68cb5b0894481c15231
Salem Harrache <salem@harrache.info>
2013-03-11 13:08:16 +0100

    clean network node

--- robair_driver/scripts/network.py
+++ robair_driver/scripts/network.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 from __future__ import division
 import roslib
 
@@ -11,8 +10,9 @@ from std_msgs.msg import String
 
 class NetworkNode():
     '''Robair network node'''
-    def __init__(self, interfaces, topic, pubrate=10):
-        rospy.init_node('battery')
+    def __init__(self, interfaces, topic, pubrate=10, node_name='network'):
+        self.node_name = node_name
+        rospy.init_node(self.node_name)
         self.pub = rospy.Publisher(topic, String)
         self.interfaces = interfaces
         self.time_sleep = 1 / pubrate if pubrate != 0 else 1
@@ -30,4 +30,6 @@ class NetworkNode():
 
 if __name__ == '__main__':
     wifi_node = NetworkNode(['wlan0', 'wlan1'], '/info/network')
+    rospy.loginfo("%s running..." % wifi_node.node_name)
     wifi_node.main_loop()
+    rospy.loginfo("%s stopped." % wifi_node.node_name)
