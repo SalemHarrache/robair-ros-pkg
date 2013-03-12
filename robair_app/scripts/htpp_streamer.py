4672cf55d0acb4a330d415a51a469fb193c565d7
Salem Harrache <salem@harrache.info>
2013-03-12 17:43:49 +0100

    we need find a way to encode h264 (mp4 video) with gstreamer

--- robair_app/scripts/htpp_streamer.py
+++ robair_app/scripts/htpp_streamer.py
@@ -1,44 +1,61 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import socket
+from select import select
 import subprocess
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+import multiprocessing
+from flask import Flask, Response
 
 
-class RequestHandler(BaseHTTPRequestHandler):
-    def _writeheaders(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'video/ogg')
-        self.end_headers()
-
-    def do_HEAD(self):
-        self._writeheaders()
-
-    def do_GET(self):
-        self._writeheaders()
-        DataChunkSize = 10000
-        command = ("gst-launch-0.10 v4l2src ! tee name=videoout ! queue !"
-                   " videorate ! 'video/x-raw-yuv,width=640,height=480,"
-                   "framerate=30/1' ! queue ! theoraenc quality=60 ! "
-                   "queue ! muxout. pulsesrc ! audio/x-raw-int,rate="
-                   "22000,channels=1,width=16 ! queue ! audioconvert"
-                   " ! vorbisenc ! queue ! muxout. oggmux name=muxout"
-                   " ! filesink location=/dev/stdout videoout.")
-
-        print("running command: %s" % (command, ))
-        p = subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=-1,
-                             shell=True)
-        print("starting polling loop.")
-        while(p.poll() is None):
-            print "looping... "
-            stdoutdata = p.stdout.read(DataChunkSize)
-            self.wfile.write(stdoutdata)
-        print("Done Looping")
-        print("dumping last data, if any")
-        stdoutdata = p.stdout.read(DataChunkSize)
-        self.wfile.write(stdoutdata)
+app = Flask(__name__)
+app.secret_key = 'blablabla'
 
 
-if __name__ == '__main__':
-    serveraddr = ('127.0.0.1', 9090)
-    print 'Httpd serve forever on http://%s:%s' % serveraddr
-    srvr = HTTPServer(serveraddr, RequestHandler)
-    srvr.serve_forever()
+@app.route('/webcam.ogg', methods=["GET"])
+def video():
+    return Response(video_stream(), mimetype='video/ogg')
+
+
+def video_stream():
+    def run_gstreamer():
+        command = ('gst-launch-0.10 videotestsrc ! queue ! videorate '
+                   ' ! "video/x-raw-yuv,width=640,'
+                   'height=480,framerate=15/1" ! queue ! theoraenc '
+                   'quality=15 ! queue ! muxout. pulsesrc ! audio/x-'
+                   'raw-int,rate=22000,channels=1,width=16 ! queue '
+                   '! audioconvert ! vorbisenc ! queue ! muxout. '
+                   'oggmux name=muxout ! tcpserversink port=9999')
+        subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=-1,
+                         shell=True)
+    gstreamer_worker = multiprocessing.Process(target=run_gstreamer)
+    gstreamer_worker.start()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('', 9999))
+    while True:
+        readable = select([s], [], [], 0.1)[0]
+        for s in readable:
+            data = s.recv(10000)
+            if not data:
+                break
+            yield data
+    gstreamer_worker.terminate()
+    gstreamer_worker.join()
+
+
+def run():
+    app.debug = False
+    app.run(port=9090, threaded=True)
+
+
+server = multiprocessing.Process(target=run)
+
+
+if __name__ == "__main__":
+    try:
+        print 'Httpd serve forever'
+        server.start()
+    except KeyboardInterrupt:
+        # server.terminate()
+        server.join()
+        print "Shutdown camera server ..."
