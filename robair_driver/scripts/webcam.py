c0ebe66cd0eac9322cde8c39e325e84cc09fb92c
Salem Harrache <salem@harrache.info>
2013-03-19 19:35:55 +0100

    run gstreamer_task in another process

--- robair_driver/scripts/webcam.py
+++ robair_driver/scripts/webcam.py
@@ -3,6 +3,7 @@
 from __future__ import division
 import roslib
 import subprocess
+from multiprocessing import Process
 
 roslib.load_manifest('robair_driver')
 
@@ -16,19 +17,27 @@ class WebcamNode():
         # TODO: publish image from gstreamer server to topic /info/image
         self.pub = rospy.Publisher(topic, String)
         rospy.init_node('webcam')
+        self.gstreamer_worker = Process(target=self._gstreamer_task)
 
-    def run_gstreamer(self):
+    def start(self):
+        self.gstreamer_worker.start()
+
+    def stop(self):
+        self.gstreamer_worker.terminate()
+        self.gstreamer_worker.join()
+
+    def _gstreamer_task(self):
         command = ('gst-launch v4l2src device=/dev/video0 ! '
                    '\'video/x-raw-yuv,width=640,height=480\' ! '
                    'x264enc pass=qual quantizer=20 tune=zerolatency ! avimux !'
                    ' tcpserversink  port=9999')
-
-        subprocess.Popen(command,
-                         stdout=subprocess.PIPE,
-                         bufsize=-1,
-                         shell=True)
+        subprocess.call(command, shell=True)
 
 
 if __name__ == '__main__':
     webcam_node = WebcamNode()
-    webcam_node.run_gstreamer()
+    rospy.loginfo("webcam running...")
+    webcam_node.start()
+    rospy.spin()
+    webcam_node.stop()
+    rospy.loginfo("webcam stopped.")
