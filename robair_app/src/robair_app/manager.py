057e211a79ed89bb5e0273e7dab95cdff158e3d7
Salem Harrache <salem@harrache.info>
2013-03-20 11:59:42 +0100

    make_reservation in client manager

--- robair_app/src/robair_app/manager.py
+++ robair_app/src/robair_app/manager.py
@@ -1,7 +1,11 @@
 import rospy
+import requests
+from robair_msgs.msg import Command
+
+from robair_common import log
+
 from .xmpp.client import ClientXMPP
 from .xmpp.rpc import remote
-from robair_msgs.msg import Command
 
 
 class RobotManager(ClientXMPP):
@@ -32,4 +36,18 @@ class ClientManager(ClientXMPP):
         rospy.init_node(node_name)
         self.robot_jid = rospy.get_param('robot_jabber_id')
         self.proxy_robot = self.get_proxy(self.robot_jid)
+        self.proxy_robot.hello(self.make_reservation())
+        # subscriber to a remote cmd
         rospy.Subscriber('/cmd', Command, self.proxy_robot.publish_cmd)
+
+    def make_reservation(self):
+        url = rospy.get_param('robair_api_url')
+        r = requests.get(url + "new", params={"jid": self.jid})
+        data = r.json()
+        if data['error']:
+            log.info("Error: %s" % data['error_message'])
+        else:
+            return data['key']
+
+    def publish_battery_state(self, data):
+        pass
