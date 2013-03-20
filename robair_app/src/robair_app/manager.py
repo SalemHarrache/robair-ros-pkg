f8b5015d0e8df0ffbddf23fae55b848800f73948
Salem Harrache <salem@harrache.info>
2013-03-20 13:15:50 +0100

    add mechanism to ckeck permission on the robot from the ressource manager website

--- robair_app/src/robair_app/manager.py
+++ robair_app/src/robair_app/manager.py
@@ -3,6 +3,7 @@ import requests
 from robair_msgs.msg import Command
 
 from robair_common import log
+from std_msgs.msg import String
 
 from .xmpp.client import ClientXMPP
 from .xmpp.rpc import remote
@@ -14,18 +15,30 @@ class RobotManager(ClientXMPP):
         jid = rospy.get_param('robot_jabber_id')
         password = rospy.get_param('robot_jabber_password')
         super(RobotManager, self).__init__(jid, password)
+        rospy.Subscriber('/info/battery', String, self.forward_distance)
         self.cmd_publisher = rospy.Publisher('/cmd', Command)
         self.clients = {}
 
     @remote
-    def publish_cmd(self, cmd):
-        self.cmd_publisher.publish(cmd)
+    def hello(self, key):
+        url = rospy.get_param('robair_api_url')
+        r = requests.get(url + "check", params={"key": key})
+        authorize = r.json()['valid']
+        if r.json()['valid']:
+            jid = self.current_rpc_session().client_jid
+            self.clients[jid] = self.get_proxy(jid)
+        return authorize
+
+    def forward_distance(self, distance):
+        for client in self.clients.items():
+            client.publish_distance(distance)
 
     @remote
-    def hello(self, key):
-        session = self.current_rpc_session()
-        self.clients[session.client_jid] = True
-        return True
+    def publish_cmd(self, cmd):
+        jid = self.current_rpc_session().client_jid
+        if jid in self.clients:
+            self.cmd_publisher.publish(cmd)
+            return True
 
 
 class ClientManager(ClientXMPP):
@@ -34,9 +47,11 @@ class ClientManager(ClientXMPP):
         password = rospy.get_param('tv_jabber_password')
         super(ClientManager, self).__init__(jid, password)
         rospy.init_node(node_name)
+        self.pub_ultrasonic = rospy.Publisher('/info/battery', String)
         self.robot_jid = rospy.get_param('robot_jabber_id')
         self.proxy_robot = self.get_proxy(self.robot_jid)
-        self.proxy_robot.hello(self.make_reservation())
+        if not self.proxy_robot.hello(self.make_reservation()):
+            raise RuntimeError('RobAir permission denied, try later...')
         # subscriber to a remote cmd
         rospy.Subscriber('/cmd', Command, self.proxy_robot.publish_cmd)
 
@@ -49,5 +64,5 @@ class ClientManager(ClientXMPP):
         else:
             return data['key']
 
-    def publish_battery_state(self, data):
-        pass
+    def publish_distance(self, distance):
+        self.pub_ultrasonic.publish(distance)
