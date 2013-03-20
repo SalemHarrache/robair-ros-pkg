167185b2d022a8d30223e1620741435bede938b0
Mickaël Nicolaccini <m.nicolaccini@gmail.com>
2013-03-20 10:20:52 +0100

    allow to manage audio stream on the player

--- robair_app/src/robair_app/player.py
+++ robair_app/src/robair_app/player.py
@@ -4,15 +4,16 @@ from PySide import QtGui
 from PySide.phonon import Phonon
 
 
-def get_widget_player(file_path):
-    media_src_rem = Phonon.MediaSource(file_path)
-    media_obj_rem = Phonon.MediaObject()
-    media_obj_rem.setCurrentSource(media_src_rem)
+def get_widget_player(file_path, audio):
+    media_src = Phonon.MediaSource(file_path)
+    media_obj = Phonon.MediaObject()
+    media_obj.setCurrentSource(media_src)
     video_widget = Phonon.VideoWidget()
-    Phonon.createPath(media_obj_rem, video_widget)
-    #audio_out = Phonon.AudioOutput(Phonon.VideoCategory)
-    #Phonon.createPath(media_obj_rem, audio_out)
-    return media_obj_rem, video_widget
+    Phonon.createPath(media_obj, video_widget)
+    if audio:
+        audio_out = Phonon.AudioOutput(Phonon.VideoCategory)
+        Phonon.createPath(media_obj, audio_out)
+    return media_obj, video_widget
 
 
 class VideoStreamPlayer(object):
@@ -22,8 +23,10 @@ class VideoStreamPlayer(object):
         self.frame = QtGui.QWidget()
         self.layout = QtGui.QGridLayout()
         self.layout.columnMinimumWidth(11)
-        self.local_media, self.local_widget = get_widget_player(local_url)
-        self.remote_media, self.remote_widget = get_widget_player(remote_url)
+        self.local_media, self.local_widget = get_widget_player(local_url,
+                                                                False)
+        self.remote_media, self.remote_widget = get_widget_player(remote_url,
+                                                                  True)
         self.local_widget.setMaximumSize(200, 150)
         self.layout.addWidget(self.remote_widget, 0, 0, 10, 10)
         self.layout.addWidget(self.local_widget, 9, 9, 1, 1)
