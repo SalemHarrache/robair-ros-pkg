287ae3c6c3467d205676f709fd55d99a2e9483fb
Salem Harrache <salem@harrache.info>
2013-03-20 10:17:19 +0100

    show http_streamer url

--- robair_app/scripts/http_streamer.py
+++ robair_app/scripts/http_streamer.py
@@ -12,8 +12,10 @@ from robair_app.http_streamer import HTTPStreamer
 if __name__ == '__main__':
     node_name = os.path.basename(__file__).strip('.py')
 
+    rospy.init_node(node_name)
     server = HTTPStreamer()
     server.start()
+    print server.url
     server.display(server.url)
 
     rospy.loginfo("%s running..." % node_name)
