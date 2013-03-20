b0b16c470707b9b6a583b9ab0255fd69c99872b9
Salem Harrache <salem@harrache.info>
2013-03-20 15:30:18 +0100

    http_stream without gstreamer_worker

--- robair_app/scripts/http_streamer.py
+++ robair_app/scripts/http_streamer.py
@@ -2,14 +2,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import socket
-from select import select
-import subprocess
-import multiprocessing
+import select
+import uuid
 from flask import Flask, Response
 
 
 app = Flask(__name__)
-app.secret_key = 'blablabla'
+app.secret_key = uuid.uuid4()
 
 
 @app.route('/', methods=["GET"])
@@ -17,45 +16,18 @@ def video():
     return Response(video_stream_tcp(), mimetype='video/mp4')
 
 
-def run_gstreamer():
-    def gstreamer_task():
-        command = ('gst-launch v4l2src device=/dev/video0 ! '
-                   '\'video/x-raw-yuv,width=640,height=480\' ! '
-                   'x264enc pass=qual quantizer=20 tune=zerolatency ! avimux !'
-                   ' tcpserversink  port=9999')
-
-        subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=-1,
-                         shell=True)
-    gstreamer_worker = multiprocessing.Process(target=gstreamer_task)
-    gstreamer_worker.start()
-    return gstreamer_worker
-
-
 def video_stream_tcp():
     buffer_size = 10000
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('', 9999))
     while True:
-        readable = select([s], [], [])
+        readable = select.select([s], [], [])
         yield readable[0][0].recv(buffer_size)
 
 
 def run():
-    gstreamer_worker = run_gstreamer()
     app.debug = False
-    app.run(port=9090, threaded=True)
-    gstreamer_worker.terminate()
-    gstreamer_worker.join()
-
-
-server = multiprocessing.Process(target=run)
-
+    app.run(port=9090, host="0.0.0.0", threaded=True)
 
 if __name__ == "__main__":
-    try:
-        print 'Httpd serve forever'
-        server.start()
-    except KeyboardInterrupt:
-        # server.terminate()
-        server.join()
-        print "Shutdown camera server ..."
+    run()
