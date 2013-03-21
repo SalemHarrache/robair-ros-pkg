640090a5cbfb7fec37ca81cb153c483f469b7036
Salem Harrache <salem@harrache.info>
2013-03-21 10:33:27 +0100

    improve httpstreamer

--- robair_app/scripts/http_streamer.py
+++ robair_app/scripts/http_streamer.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import os
 import socket
 import select
 import uuid
+import subprocess
+import multiprocessing
 from flask import Flask, Response
 
+import roslib
+import rospy
+
+roslib.load_manifest('robair_app')
 
 app = Flask(__name__)
 app.secret_key = uuid.uuid4()
@@ -14,6 +21,20 @@ app.secret_key = uuid.uuid4()
 @app.route('/', methods=["GET"])
 def video():
     return Response(video_stream_tcp(), mimetype='video/mp4')
+
+
+def run_gstreamer():
+    def gstreamer_task():
+        device = rospy.get_param('webcam_device', '/dev/video0')
+        command = ('gst-launch v4l2src device=%s ! '
+                   '\'video/x-raw-yuv,width=640,height=480\' ! '
+                   'x264enc pass=cbr quantizer=20 tune=zerolatency ! avimux !'
+                   ' tcpserversink  port=9999' % device)
+        subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=-1,
+                         shell=True)
+    gstreamer_worker = multiprocessing.Process(target=gstreamer_task)
+    gstreamer_worker.start()
+    return gstreamer_worker
 
 
 def video_stream_tcp():
@@ -25,9 +46,17 @@ def video_stream_tcp():
         yield readable[0][0].recv(buffer_size)
 
 
-def run():
+def run(node_name):
+    rospy.init_node(node_name)
+    gstreamer_worker = run_gstreamer()
     app.debug = False
     app.run(port=9090, host="0.0.0.0", threaded=True)
+    gstreamer_worker.terminate()
+    gstreamer_worker.join()
 
-if __name__ == "__main__":
-    run()
+
+if __name__ == '__main__':
+    node_name = os.path.basename(__file__).strip('.py')
+    rospy.loginfo("%s running..." % node_name)
+    run(node_name)
+    rospy.loginfo("%s stopped." % node_name)
